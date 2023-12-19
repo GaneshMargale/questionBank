2a22cb3016b9bb656d7f962fac6926a1d5f12500
Ganesh Sanjay Margale <108195526+GaneshMargale@users.noreply.github.com>
2023-12-20 00:10:14 +0530

    Add files via upload

---
+++ Model/Logical3Model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Logical3Schema = new mongoose.Schema({
+  Questions: [
+    {
+      questionDescription: {
+        type: String,
+        required: [true, 'Provide the question description'],
+      },
+      options: {
+        A: { type: String, required: [true, 'Provide option A'] },
+        B: { type: String, required: [true, 'Provide option B'] },
+        C: { type: String, required: [true, 'Provide option C'] },
+        D: { type: String, required: [true, 'Provide option D'] },
+      },
+      contributor: {
+        type: String,
+        required: [true, 'Provide the name of the contributor'],
+      },
+    },
+  ],
+});
+
+const Logical3 = mongoose.model('Logical3', Logical3Schema);
+
+module.exports = Logical3;
---
+++ Model/Quant2Model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Quant2Schema = new mongoose.Schema({
+  Questions: [
+    {
+      questionDescription: {
+        type: String,
+        required: [true, 'Provide the question description'],
+      },
+      options: {
+        A: { type: String, required: [true, 'Provide option A'] },
+        B: { type: String, required: [true, 'Provide option B'] },
+        C: { type: String, required: [true, 'Provide option C'] },
+        D: { type: String, required: [true, 'Provide option D'] },
+      },
+      contributor: {
+        type: String,
+        required: [true, 'Provide the name of the contributor'],
+      },
+    },
+  ],
+});
+
+const Quant2 = mongoose.model('Quant2', Quant2Schema);
+
+module.exports = Quant2;
